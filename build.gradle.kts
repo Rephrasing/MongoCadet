4444b26ca0815867a47581c094931d758e7a724c
Rephrasing <iamnotifrankieee8282@gmail.com>
2023-07-03 06:14:57 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    `maven-publish`
+}
+
+group = "io.github.rephrasing"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
+    // https://mvnrepository.com/artifact/org.mongodb/mongodb-driver-sync
+    implementation("org.mongodb:mongodb-driver-sync:4.10.1")
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar").configure {
+
+    minimize()
+    archiveFileName.set("${project.name}-v${project.version}.jar")
+    destinationDirectory.set(file("$rootDir/output"))
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/java/io/github/rephrasing/mongocadet/MongoCadetLoader.java
@@ -0,0 +1,41 @@
+package io.github.rephrasing.mongocadet;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MongoCadetLoader extends JavaPlugin {
+
+    private MongoClient client;
+    private static MongoCadetLoader instance;
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        String connection = getConfig().getString("mongo-connection-string");
+
+        if (connection == null || connection.isEmpty()) {
+            getLogger().severe("Could not find a mongo-connection-string value in plugin configuration. cannot connect to MongoDB");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        client = MongoClients.create(connection);
+        instance = this;
+        getLogger().info("Successfully connected to MongoDB");
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+        client.close();
+        getLogger().info("Successfully shutdown");
+    }
+
+    public static MongoCadetLoader getInstance() {
+        return instance;
+    }
+
+    public MongoClient getMongoClient()
+    {
+        return client;
+    }
+}
